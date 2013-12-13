49c43b99b71bf616fe4be84d78e9ccd744c3e339
zhouwei <leonzhouwei@gmail.com>
2013-12-13 21:34:08 +0800

    Item.parse() works

--- org.openhab.designerfx.server/src/main/java/org/openhab/designerfx/server/businesslogic/domainmodel/internal/Item.java
+++ org.openhab.designerfx.server/src/main/java/org/openhab/designerfx/server/businesslogic/domainmodel/internal/Item.java
@@ -5,7 +5,17 @@ import java.util.List;
 import org.openhab.designerfx.server.common.Constants;
 import org.openhab.designerfx.server.common.DesignerFXException;
 
+import com.google.common.collect.Lists;
+
 public class Item {
+	
+	public class InvalidParserStringException extends DesignerFXException {
+		private static final long serialVersionUID = -6306477045323118068L;
+
+		public InvalidParserStringException(String string) {
+			super(string + " is not correct");
+		}
+	}
 
 	public class ItemNameInUseException extends DesignerFXException {
 		private static final long serialVersionUID = -6306477045323118068L;
@@ -15,28 +25,75 @@ public class Item {
 		}
 	}
 
-	private String type;
-	private String name;
-	private String labelText;
-	private String iconName;
-	private List<String> groups;
-	private String bindingConfig;
+	private String type; // mandatory
+	private String name; // mandatory
+	private String labelText; // optional
+	private String iconName; // optional
+	private List<String> groups = Lists.newArrayList(); // optional
+	private String bindingConfig; // optional
 
 	public static Item parse(String line) {
-		// parse the type
-		int typeEndIndex = line.indexOf(Constants.STRING_SPACE);
-		final String type = line.substring(0, typeEndIndex);
-		// parse the name
-		line = line.substring(typeEndIndex).trim();
-		int nameEndIndex = line.indexOf(Constants.STRING_SPACE);
-		final String name = line.substring(0, nameEndIndex);
-		// parse the label text
-		// parse the icon name
-		// parse the groups
-		// parse the binding config
 		Item item = new Item();
+		// parse the type
+		int index = -1;
+		line = line.trim();
+		index = line.indexOf(Constants.STRING_SPACE);
+		final String type = line.substring(0, index);
 		item.setType(type);
+		// parse the name
+		if (index > line.length() - 1) {
+			return item;
+		}
+		line = line.substring(index).trim();
+		index = line.indexOf(Constants.STRING_SPACE);
+		if (index == -1) {
+		}
+		final String name = line.substring(0, index);
 		item.setName(name);
+		// parse the label text if exists
+		if (index > line.length() - 1) {
+			return item;
+		}
+		line = line.substring(index).trim();
+		if (line.startsWith("\"")) {
+			index = line.indexOf("\"", 1);
+			final String labelText = line.substring(1, index);
+			item.setLabelText(labelText);
+			index = index + 1;
+		}
+		// parse the icon name if exists
+		if (index > line.length() - 1) {
+			return item;
+		}
+		line = line.substring(index).trim();
+		if (line.startsWith("<")) {
+			index = line.indexOf(">", 1);
+			final String iconName = line.substring(1, index);
+			item.setIconName(iconName);
+			index = index + 1;
+		}
+		// parse the groups if exists
+		if (index > line.length() - 1) {
+			return item;
+		}
+		line = line.substring(index).trim();
+		if (line.startsWith("(")) {
+			index = line.indexOf(")", index + 1);
+			final String s = line.substring(1, index);
+			String[] groups = s.split(",");
+			item.addGroups(groups);
+			index = index + 1;
+		}
+		// parse the binding config if exists
+		if (index > line.length() - 1) {
+			return item;
+		}
+		line = line.substring(index).trim();
+		if (line.startsWith("{")) {
+			index = line.indexOf("}", index + 1);
+			final String bindingConfig = line.substring(1, index);
+			item.setBindingConfig(bindingConfig);
+		}
 		return item;
 	}
 
@@ -66,31 +123,66 @@ public class Item {
 
 	@Override
 	public String toString() {
-		return type + " " + name;
+		// itemtype itemname ["labeltext"] [<iconname>] [(group1, group2, ...)] [{bindingconfig}]
+		StringBuilder sb = new StringBuilder();
+		sb.append(type);
+		sb.append(Constants.STRING_SPACE);
+		sb.append(name);
+		if (labelText != null) {
+			sb.append(Constants.STRING_SPACE);
+			sb.append("\"");
+			sb.append(labelText);
+			sb.append("\"");
+		}
+		if (iconName != null) {
+			sb.append(Constants.STRING_SPACE);
+			sb.append("<");
+			sb.append(iconName);
+			sb.append(">");
+		}
+		if (!groups.isEmpty()) {
+			sb.append(Constants.STRING_SPACE);
+			sb.append("(");
+			for (String group : groups) {
+				sb.append(group);
+				sb.append(",");
+			}
+			sb.deleteCharAt(sb.length() - 1);
+			sb.append(")");
+		}
+		if (bindingConfig != null) {
+			sb.append(Constants.STRING_SPACE);
+			sb.append("{");
+			sb.append(bindingConfig);
+			sb.append("}");
+		}
+		return sb.toString();
 	}
 
 	public void setType(String type) {
-		this.type = type;
+		this.type = type.trim();
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		this.name = name.trim();
 	}
 
 	public void setLabelText(String labelText) {
-		this.labelText = labelText;
+		this.labelText = labelText.trim();
 	}
 
 	public void setIconName(String iconName) {
-		this.iconName = iconName;
+		this.iconName = iconName.trim();
 	}
 
-	public void setGroups(List<String> groups) {
-		this.groups = groups;
+	public void addGroups(String[] groups) {
+		for (String group : groups) {
+			this.groups.add(group.trim());
+		}
 	}
 
 	public void setBindingConfig(String bindingConfig) {
-		this.bindingConfig = bindingConfig;
+		this.bindingConfig = bindingConfig.trim();
 	}
 
 }
--- org.openhab.designerfx.server/src/test/java/org/openhab/designerfx/server/businesslogic/domainmodel/internal/ItemTest.java
+++ org.openhab.designerfx.server/src/test/java/org/openhab/designerfx/server/businesslogic/domainmodel/internal/ItemTest.java
@@ -1,13 +1,15 @@
 package org.openhab.designerfx.server.businesslogic.domainmodel.internal;
 
 import org.junit.Test;
+import org.openhab.designerfx.server.util.Util;
 
 public class ItemTest {
 
 	@Test
 	public void test() {
-		String line = "Switch Light_Room_Table \"Table Light\" (Room, Lights) { knx = \"<0/1/10+0/1/30\"}";
+		String line = "Switch Light_Room_Table \"Table Light\" <iconname> ";
 		Item item = Item.parse(line);
+		Util.printSeparateLine();
 		System.out.println(item);
 	}
 
