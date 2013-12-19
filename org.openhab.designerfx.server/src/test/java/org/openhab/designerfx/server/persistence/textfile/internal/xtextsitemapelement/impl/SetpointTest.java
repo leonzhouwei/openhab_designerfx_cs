f5bd6243dcb02cc1b9ccafcd277d08e9acf9579d
zhouwei <leonzhouwei@gmail.com>
2013-12-19 22:17:49 +0800

    Setpoint.xtext() unit test DONE

---
+++ org.openhab.designerfx.server/src/test/java/org/openhab/designerfx/server/persistence/textfile/internal/xtextsitemapelement/impl/SetpointTest.java
@@ -0,0 +1,18 @@
+package org.openhab.designerfx.server.persistence.textfile.internal.xtextsitemapelement.impl;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class SetpointTest {
+
+	@Test
+	public void testParse() {
+		final String expected = "Setpoint item=Temperature_Setpoint minValue=16 maxValue=28 step=0.5";
+		Setpoint sitemapElement = Setpoint.parse(expected);
+		String actual = sitemapElement.toXtext();
+		assertThat(actual, Matchers.equalTo(expected));
+	}
+
+}
