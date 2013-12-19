7adf7c2acd18976dcbd47247bb44fbd16f8139d2
zhouwei <leonzhouwei@gmail.com>
2013-12-19 22:14:56 +0800

    Image.xtext() unit test DONE

---
+++ org.openhab.designerfx.server/src/test/java/org/openhab/designerfx/server/persistence/textfile/internal/xtextsitemapelement/impl/ImageTest.java
@@ -0,0 +1,18 @@
+package org.openhab.designerfx.server.persistence.textfile.internal.xtextsitemapelement.impl;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class ImageTest {
+
+	@Test
+	public void testParse() {
+		final String expected = "Image url=\"http://localhost:8080/images/splash-ipad-h.png\"";
+		Image sitemapElement = Image.parse(expected);
+		String actual = sitemapElement.toXtext();
+		assertThat(actual, Matchers.equalTo(expected));
+	}
+
+}
