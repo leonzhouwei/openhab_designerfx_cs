863ddb9a0bc9ea7eba7c31a19e663163d28915e6
zhouwei <leonzhouwei@gmail.com>
2013-12-19 22:11:05 +0800

    rename some local variables

--- org.openhab.designerfx.server/src/test/java/org/openhab/designerfx/server/persistence/textfile/internal/xtextsitemapelement/impl/ChartTest.java
+++ org.openhab.designerfx.server/src/test/java/org/openhab/designerfx/server/persistence/textfile/internal/xtextsitemapelement/impl/ChartTest.java
@@ -10,8 +10,8 @@ public class ChartTest {
 	@Test
 	public void testParse() {
 		final String expected = "Chart item=Weather_Chart period=h refresh=600 visibility=[Weather_Chart_Period==0, Weather_Chart_Period==\"Uninitialized\"]";
-		Chart chart = Chart.parse(expected);
-		String actual = chart.toXtext();
+		Chart sitemapElement = Chart.parse(expected);
+		String actual = sitemapElement.toXtext();
 		assertThat(actual, Matchers.equalTo(expected));
 	}
 
