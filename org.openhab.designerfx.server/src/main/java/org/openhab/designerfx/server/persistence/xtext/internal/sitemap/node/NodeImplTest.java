cce42bd5ae9104d087222a268f2c4872b4033c55
zhouwei <leonzhouwei@gmail.com>
2013-12-20 20:21:42 +0800

    Node.toXtext(String) has passed the unit test

--- org.openhab.designerfx.server/src/main/java/org/openhab/designerfx/server/persistence/xtext/internal/sitemap/node/NodeImplTest.java
+++ org.openhab.designerfx.server/src/main/java/org/openhab/designerfx/server/persistence/xtext/internal/sitemap/node/NodeImplTest.java
@@ -1,6 +1,10 @@
 package org.openhab.designerfx.server.persistence.xtext.internal.sitemap.node;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.openhab.designerfx.server.common.Constants;
 import org.openhab.designerfx.server.persistence.xtext.internal.sitemap.node.atom.Frame;
 import org.openhab.designerfx.server.persistence.xtext.internal.sitemap.node.atom.Group;
 import org.openhab.designerfx.server.persistence.xtext.sitemap.Atom;
@@ -9,12 +13,28 @@ public class NodeImplTest {
 
 	@Test
 	public void testToXtext() {
-		Atom rootAtom = Frame.parse("Frame label=\"aFrame\"");
+		final String frame = "Frame label=\"aFrame\"";
+		final String group = "Group item=gFF label=\"First Floor\" icon=\"firstfloor\"";
+		final String indentation = "    ";
+		StringBuilder sb = new StringBuilder();
+		sb.append(indentation);
+		sb.append(frame);
+		sb.append(" {");
+		sb.append(Constants.LINE_SEPARATOR);
+		sb.append(indentation);
+		sb.append(indentation);
+		sb.append(group);
+		sb.append(Constants.LINE_SEPARATOR);
+		sb.append(indentation);
+		sb.append("}");
+		final String expected = sb.toString();
+		Atom rootAtom = Frame.parse(frame);
 		NodeImpl rootNode = new NodeImpl(rootAtom);
-		Atom childAtom = Group.parse("Group item=gFF label=\"First Floor\" icon=\"firstfloor\"");
+		Atom childAtom = Group.parse(group);
 		NodeImpl childNode = new NodeImpl(childAtom);
 		rootNode.addChild(childNode);
-		System.out.println(rootNode.toXtext("\t"));
+		final String actual = rootNode.toXtext(indentation);
+		assertThat(actual, Matchers.equalTo(expected));
 	}
 
 }
